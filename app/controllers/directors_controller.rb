9be59548ad489b4bb61f09e5b056eeb05a023e05
Kyle Baum <username@users.noreply.github.com>
2016-11-06 14:41:42 -0600

    save #1

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,45 @@
+class DirectorsController < ApplicationController
+  def index
+    @directors = Director.all
+  end
+
+  def show
+    @director = Director.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+  end
+
+  def create_row
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    render("show")
+  end
+
+  def edit_form
+    @director = Director.find(params[:id])
+  end
+
+  def update_row
+    @director = Director.find_by({ :id => params[:id] })
+    @director.dob = params[:the_dob]
+    @director.name = params[:the_name]
+    @director.bio = params[:the_bio]
+    @director.image_url = params[:the_image_url]
+    @director.save
+
+    render("show")
+  end
+
+  def destroy
+    @director = Director.find_by({ :id => params[:id] })
+    @director.destroy
+
+  end
+end
